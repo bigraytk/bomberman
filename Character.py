806e9b645194c3d17f089dea40044430963dff83
raymond takor <raymond.takor@gmail.com>
2019-01-18 20:49:46 -0800

    I've added additional constants for characters and keyboad input.  The move method for grid based logic has also been added. -Ray

--- Character.py
+++ Character.py
@@ -1,5 +1,8 @@
 
 import Bomb
+import level
+import Wall
+import constants
 
 class Character(object):
 
@@ -8,8 +11,52 @@ class Character(object):
     PlayerCharacter and Emeny are subclasses
     '''
 
-    class PlayerCharacter(Character):
+    def __init__(self,x,y,facing,kind):
+        '''Constructor'''
 
+        self.x = x
+        self.y = y
+        self.xres = x#init this by running self.x through the grid_to_res conversion function
+        self.yres = y#same but for y
+        self.facing = facing
+        constants.TILE_WALL
+        if kind == constants.PC:
+            self.Player = self.PlayerCharacter()
+        elif kind == constants.ENEMY:
+            self.Enemy = self.Enemy()
+        else:
+            raise RuntimeError(kind + ' is not a valid kind of character')
+
+
+    def move(self,direction,layout):
+        '''
+        Controls movement of a character. Takes a direcetion as input, if character
+        is able to move in that direction, will update the character's position and 
+        facing. Else, will just update facing.
+        ''' 
+        if constants.UP == direction:
+            if (self.y - constants.GRID_MOVE_SCALE) >= 0 and (not isinstance(layout[self.x][self.y - constants.GRID_MOVE_SCALE], Wall)):
+                self.y -= constants.GRID_MOVE_SCALE
+
+        elif constants.DOWN == direction:
+            if (self.y + constants.GRID_MOVE_SCALE) <= constants.MAP_HEIGHT and (not isinstance(layout[self.x][self.y + constants.GRID_MOVE_SCALE], Wall)):
+                self.y += constants.GRID_MOVE_SCALE
+        
+        elif constants.LEFT == direction:
+            if (self.x - constants.GRID_MOVE_SCALE) >= 0 and (not isinstance(layout[self.x - constants.GRID_MOVE_SCALE][self.y], Wall)):
+                self.x -= constants.GRID_MOVE_SCALE
+
+        elif constants.RIGHT == direction:
+            if (self.x + constants.GRID_MOVE_SCALE) <= constants.MAP_HEIGHT and (not isinstance(layout[self.x + constants.GRID_MOVE_SCALE][self.y], Wall)):
+                self.x += constants.GRID_MOVE_SCALE
+
+        #checks if able to move in direction
+        #if no, stays in same square, but update self.facing
+        #if yes, move to correct square and update self.facing
+
+
+    class PlayerCharacter(Character):
+        
         '''
         This object is for the player's character. Only one
         should be instantiated.
@@ -41,11 +88,11 @@ class Character(object):
             '''This method is called when the PC occupies the same space as a 
             powerup. 
             '''
-            if powerup == RANGE and self.bombRange < 5:
+            if powerup == constants.RANGE and self.bombRange < 5:
                 self.bombRange += 1
-            elif powerup == COUNT and self.bombCount < 5:
+            elif powerup == constants.COUNT and self.bombCount < 5:
                 self.bombCount += 1
-            elif powerup == BOOT:
+            elif powerup == constants.BOOT:
                 self.boot = True
 
 
@@ -60,45 +107,17 @@ class Character(object):
         def __init__(self,version):
             '''Constructor'''
             Character.__init__(self)
-            if version == BASIC: #BASIC is some value that we have not mapped yet
-                self.speed = LOW
-                self.logic = RANDOM
-            elif version == MED: 
-                self.speed = LOW
-                self.logic = BASIC
-            elif version == ADVANCED:
-                self.speed = HIGH
-                self.logic = SMART
+            if version == constants.BASIC: #BASIC is some value that we have not mapped yet
+                self.speed = constants.LOW
+                self.logic = constants.RANDOM
+            elif version == constants.MED: 
+                self.speed = constants.LOW
+                self.logic = constants.BASIC
+            elif version == constants.ADVANCED:
+                self.speed = constants.HIGH
+                self.logic = constants.SMART
             
 
         def destroy(self):
             #gets called if something destroys an enemy
             pass
-
-
-    def __init__(self,x,y,facing,kind):
-        '''Constructor'''
-
-        self.x = x
-        self.y = y
-        self.xres = x#init this by running self.x through the grid_to_res conversion function
-        self.yres = y#same but for y
-        self.facing = facing
-        if kind == PC:
-            self.Player = self.PlayerCharacter()
-        elif kind == ENEMY:
-            self.Enemy = self.Enemy()
-        else:
-            raise RuntimeError(kind + ' is not a valid kind of character')
-
-
-    def move(self,direction,layout):
-        '''
-        Controls movement of a character. Takes a direcetion as input, if character
-        is able to move in that direction, will update the character's position and 
-        facing. Else, will just update facing.
-        ''' 
-        #checks if able to move in direction
-        #if no, stays in same square, but update self.facing
-        #if yes, move to correct square and update self.facing
-
--- constants.py
+++ constants.py
@@ -32,3 +32,29 @@ SCREEN_OFFSET_X_LEFT = 32
 SCREEN_OFFSET_Y_TOP = 32
 SCREEN_OFFSET_X_RIGHT = 32
 SCREEN_OFFSET_Y_BOTTOM = 32
+
+# Enemy Constants
+BASIC = 0
+RANDOM = 1
+ADVANCED = 2
+SMART = 3
+HIGH = 4
+MED = 5
+LOW = 6
+
+# Power Up Constants
+RANGE = 0
+COUNT = 1
+BOOT = 2
+
+#character constants
+PC = 0
+ENEMY = 1
+GRID_MOVE_SCALE = 1
+RES_MOVE_SCALE = 5
+
+#direction constants
+UP = 0
+DOWN = 1
+RIGHT = 2
+LEFT = 3
