f86e25aa8645464ca130eb02601887980372f632
raymond takor <raymond.takor@gmail.com>
2019-03-17 18:48:13 -0700

    Completed Setter Getters

--- StatusBar.py
+++ StatusBar.py
@@ -20,7 +20,57 @@ class StatusBar(object):
         self.resy = const.SCREEN_OFFSET_Y_TOP + self.y * const.TILE_SIZE
 
         self.spriteIcon = pygame.sprite.Group()
-    
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @property
+    def xres(self):
+        return self.__xres
+
+    @property
+    def yres(self):
+        return self.__yres
+
+    @property
+    def spriteIcon(self):
+        return self.__spriteIcon
+
+    @spriteIcon.setter
+    def spriteIcon(self, val):
+        if val == None:
+            raise RuntimeError('Invalid Assignment')
+        self.__spriteIcon = val
+
+    @x.setter
+    def x(self, val):
+        if val < 0:
+            raise RuntimeError('Value is less than 0')
+        self.__x = val
+
+    @y.setter
+    def y(self,val):
+        if val < 0:
+            raise RuntimeError('Value is less than 0')
+        self.__y = val
+
+    @xres.setter
+    def xres(self,val):
+        if val < 0:
+            raise RuntimeError('Value is less than 0')
+        self.__xres = val
+
+    @yres.setter
+    def yres(self,val):
+        if val < 0:
+            raise RuntimeError('Value is less than 0')
+        self.__yres = val
+
     
     def addIcon(self, imageFileName, iconNum, alpha, scale = const.ICON_SCALE):
         x = const.ICON_X + const.ICON_SPACING * iconNum
